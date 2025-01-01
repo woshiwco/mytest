bd8bde8d0f4ac38523a9be8c059ae596ba1e4407
王创创 <1075379535@qq.com>
2025-01-01 15:31:57 +0000

    change from workspace vscode

--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -1,5 +1,11 @@
 public class Test {
     public static void main(String[] args) {
+        // run in workspace vs code 
         System.out.println("hello world.");
+        
+    }
+    // add function
+    publ ic add fun(int a, int b) {
+
     }
 }
